2b9d83fe030accc242bbc445db7f8b5fa17abf8c
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-03-16 05:07:10 +0600

    show Previous Submissions

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
@@ -126,7 +126,8 @@ document.querySelector("#submit").addEventListener("click", () => {
         },
       ])
     );
-  } else {
+  } 
+  else {
     localStorage.setItem(
       "results",
       JSON.stringify([
